0e3cf57311dae33569625848661d92ec5864964b
yankur <42523950+yankur@users.noreply.github.com>
2019-10-18 19:36:45 +0300

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,5 +1,54 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    
+
+    private double avgTemp;
+    private double devTemp;
+    private double minTemp;
+    private double maxTemp;
+
+
+
+    public TempSummaryStatistics(double avgTemp, double devTemp,
+                                 double minTemp, double maxTemp) {
+        this.avgTemp = avgTemp;
+        this.devTemp = devTemp;
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+    }
+
+    public TempSummaryStatistics() {
+    }
+
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+
+    public void setAvgTemp(double avg) {
+        this.avgTemp = avg;
+    }
+
+    public double getDevTemp() {
+        return devTemp;
+    }
+
+    public void setDevTemp(double dev) {
+        this.avgTemp = dev;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(double min) {
+        this.minTemp = min;
+    }
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(double max) {
+        this.maxTemp = max;
+    }
 }
